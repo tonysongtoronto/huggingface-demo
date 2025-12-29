0f86c3a8b25d3d6bbe26e17c6b7590db0279eb99
tony <songjf@yahoo.com>
2025-12-29 03:37:35 -0500

    main updates

--- backend/logger.js
+++ backend/logger.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 
-// 获取当前文件的目录路径（ES modules）
+// 获取当前文件的目录路径（ES modules）main updates
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
